645388f9996bc6dab78fe6d4608dbb17b48fe480
Frank <franklaercio@gmail.com>
2023-06-27 19:03:31 -0300

    feat: adding fetch suggestions

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    API_URL: process.env.API_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 module.exports = nextConfig;
--- src/app/suggestion/list/page.tsx
+++ src/app/suggestion/list/page.tsx
@@ -1,49 +1,74 @@
 "use client";
-
-import SuggestionCard from "@/components/SuggestionCard";
-import { useRouter } from "next/navigation";
 import { Facebook } from "lucide-react";
+import SuggestionCardList from "@/components/SuggestionCardList";
 
-export default function Suggestions() {
-  const router = useRouter();
+async function getSuggestions() {
+  const data = await fetch(process.env.API_URL + "/suggestion");
+
+  if (!data.ok) {
+    return [];
+  }
+
+  const suggestions = await data.json();
+
+  const suggestionsFormated = await Promise.all(
+    suggestions.suggestions.map(async (suggestion: any) => {
+      const userName = await getUserName(suggestion.authorId);
+      const categoryName = await getCategoryName(suggestion.categoryId);
+
+      return {
+        ...suggestion,
+        userName: userName,
+        categoryName: categoryName,
+        dataFormated: new Date(suggestion.createdAt).toLocaleDateString(
+          "pt-BR",
+          {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }
+        ),
+      };
+    })
+  );
+
+  return suggestionsFormated;
+}
+
+async function getUserName(authorId: string) {
+  const data = await fetch(process.env.API_URL + "/user/" + authorId);
+  const user = await data.json();
+  return user.data.name;
+}
+
+async function getCategoryName(categoryId: string) {
+  const data = await fetch(process.env.API_URL + "/category/" + categoryId);
+  const category = await data.json();
+  return category.data.name;
+}
+
+export default async function Suggestions() {
+  const data = await getSuggestions();
 
   return (
-    <main className="mt-2 max-w-screen-xl m-auto h-screen p-8 md:mt-8">
-      <div className="flex flex-row justify-between">
+    <main className="mt-2 max-w-screen-xl m-auto h-screen p-4 sm:p-6 md:p-8">
+      <div className="inline-table justify-between sm:flex flex-row">
         <div className="flex flex-col font-sans font-bold md:flex text-2xl">
           <p>Olá, Frank!</p>
           <p>Acompanhe as propostas da sua comunidade.</p>
         </div>
-        <div className="flex items-center">
+        <div className="hidden md:flex items-center">
           <button
             className="flex flex-row items-center align-middle rounded-md bg-white border-0 p-2 box-border h-8"
             type="button"
-            onClick={() => router.push("/login")}
           >
             <Facebook className="pr-3 text-blue" size={40} />
             <span className="font-sans font-bold text-black">COMUNIDADE</span>
           </button>
         </div>
       </div>
-      <div className="mt-12">
-        <SuggestionCard
-          title="Modernização da Área de Lazer"
-          resume="Propõe-se investir na modernização da área de lazer do condomínio, com o objetivo de oferecer mais opções de entretenimento e lazer para os moradores."
-          author="Frank Laércio"
-          date="2023/06/26"
-        />
-        <SuggestionCard
-          title="Implantação de Medidas de Sustentabilidade"
-          resume="Nesta proposta, sugere-se a implementação de medidas de sustentabilidade para reduzir os impactos ambientais do condomínio e promover uma consciência ecológica entre os moradores."
-          author="Frank Laércio"
-          date="2023/06/26"
-        />
-        <SuggestionCard
-          title="Melhorias na Segurança e Monitoramento"
-          resume="Esta proposta visa aprimorar a segurança e o monitoramento do condomínio, garantindo um ambiente tranquilo e seguro para os moradores."
-          author="Frank Laércio"
-          date="2023/06/26"
-        />
+      <div className="pt-12 pb-12">
+        <SuggestionCardList suggestions={data} />
       </div>
     </main>
   );
